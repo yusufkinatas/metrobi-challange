e02049f34ff857b7fc39a233b1ef4bf3e9e4601d
Yusuf Kınataş <yusufkinatas@gmail.com>
2019-11-20 15:34:22 +0300

    q2

--- q2.js
+++ q2.js
@@ -1,15 +1,28 @@
 const printItemsSlowly = async arr => {
-  const waitXSeconds = secs => {
-    return new Promise(resolve => setTimeout(resolve, secs * 1000));
-  };
-
   let seconds = 1;
   for (let i = 0; i < arr.length; i++) {
-    console.log(`waiting ${seconds} seconds`);
-    await waitXSeconds(seconds);
-    console.log(arr[i]);
+    // console.log(`waiting ${seconds} seconds`);
+    setTimeout(() => {
+      console.log(arr[i]);
+    }, seconds * 1000);
     seconds *= 2;
   }
 };
 
 printItemsSlowly(["a", "b", "c", "d"]);
+
+// const printItemsSlowly = async arr => {
+//   const waitXSeconds = secs => {
+//     return new Promise(resolve => setTimeout(resolve, secs * 1000));
+//   };
+
+//   let seconds = 1;
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(`waiting ${seconds} seconds`);
+//     await waitXSeconds(seconds);
+//     console.log(arr[i]);
+//     seconds *= 2;
+//   }
+// };
+
+// printItemsSlowly(["a", "b", "c", "d"]);
